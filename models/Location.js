b9dd0afdee3a51e29e501b52d26bc4a5c086f1b7
Ryan <31742593+TabooGryphon@users.noreply.github.com>
2018-11-13 10:54:16 -0600

    Delete

--- models/Location.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema;
-
-const Location = new mongoose.Schema({
-  room: {type:String, trim:true, default:''},
-  capacity: {type: Number, trim: true, default:''}
-})
-
-module.exports = mongoose.model('Location', Location)
