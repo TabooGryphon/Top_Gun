4bb3254d59806e1b14323bc6784157ae2edced64
Ryan <31742593+TabooGryphon@users.noreply.github.com>
2018-11-13 10:51:09 -0600

    Initial Commit

--- routes/admin.js
+++ routes/admin.js
@@ -3,10 +3,11 @@ const turbo = require('turbo360')({site_id: process.env.TURBO_APP_ID})
 const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID})
 const router = vertex.router()
 
-const index_controller = require('../controllers/indexController');
+const admin_controller = require('../controllers/adminController');
+// Routes for admin functions
 
-// Homepage
-router.get('/', index_controller.main);
+router.get('/', admin_controller.main);
+
 
 
 module.exports = router
