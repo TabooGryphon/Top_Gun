5588280804269ca0fe17fd2cfd9163e669e730de
Ryan <31742593+TabooGryphon@users.noreply.github.com>
2018-11-13 10:48:42 -0600

    Initial Create of Project

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 // Full Documentation - https://www.turbo360.co/docs
 const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID})
 
+
 const config = {
 	views: 'views', 		// Set views directory 
 	static: 'public', 		// Set static assets directory
 	db: { 					// Database configuration. Remember to set env variables in .env file: MONGODB_URI, PROD_MONGODB_URI
-		url: 'mongodb://localhost/top_gun',
+		url: 'mongodb://admin:password2@ds255403.mlab.com:55403/twist',
 		type: 'mongo',
 		onError: (err) => {
 			console.log('DB Connection Failed!')
@@ -19,13 +20,14 @@ const config = {
 const app = vertex.app(config) // initialize app with config options
 
 
+
 // import routes
 const index = require('./routes/index')
-const register = require('./routes/register')
+const admin = require('./routes/admin')
 
 // set routes
 app.use('/', index)
-app.use('/register', register) // sample API Routes
+app.use('/admin', admin)
 
 
 module.exports = app
