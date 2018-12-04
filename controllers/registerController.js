ef1dff53a55e67f55f6382d50d84e7199ff4a914
Ryan <ryanasmith@ksu.edu>
2018-12-04 10:33:17 -0600

    commit

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -20,30 +20,56 @@ exports.register_get = function(req, res, next){
         .sort({name: "asc"})
         .then(function(topics){
           res.render('register', {schools, topics})
+          var ryan = new Date;
+          console.log(ryan);
         })
       })
   }
 
 // Register Create Student Post
 exports.register_post = [
+
   // Validate fields.
-  body('firstName', 'Please enter your First Name.').isLength({ min: 1 }).trim(),
-  body('lastName', 'Please enter your Last Name.').isLength({ min: 1 }).trim(),
-  body('address', 'Please enter your Address.').isLength({ min: 1 }).trim(),
-	body('school', 'Please select your School').isLength({ min: 1 }).trim(),
-	body('email', 'Please enter your E-Mail.').isLength({ min: 1 }).trim(),
-	body('phone', 'Please enter your Phone Number.').isLength({ min: 1 }).trim(),
-	body('choice1', 'Please make fill all choices').isLength({ min: 1 }).trim(),
-	body('choice2', 'Please make fill all choices').isLength({ min: 1 }).trim(),
-	body('choice3', 'Please make fill all choices').isLength({ min: 1 }).trim(),
-	body('choice4', 'Please make fill all choices').isLength({ min: 1 }).trim(),
-	body('choice5', 'Please make fill all choices').isLength({ min: 1 }).trim(),
+  //body('firstName', 'Please enter your First Name.').isLength({ min: 1 }).trim(),
+  //body('lastName', 'Please enter your Last Name.').isLength({ min: 1 }).trim(),
+	//body('school', 'Please select your School').isLength({ min: 1 }).trim(),
+	//body('email', 'Please enter your E-Mail.').isLength({ min: 1 }).trim(),
+	//body('phone', 'Please enter your Phone Number.').isLength({ min: 1 }).trim(),
+	//body('choice1', 'Please make fill all choices').isLength({ min: 1 }).trim(),
+	//body('choice2', 'Please make fill all choices').isLength({ min: 1 }).trim(),
+	//body('choice3', 'Please make fill all choices').isLength({ min: 1 }).trim(),
+	//body('choice4', 'Please make fill all choices').isLength({ min: 1 }).trim(),
+	//body('choice5', 'Please make fill all choices').isLength({ min: 1 }).trim(),
 
   // Sanitize fields.
   sanitizeBody('firstName').trim().escape(),
   sanitizeBody('lastName').trim().escape(),
   sanitizeBody('address').trim().escape(),
   sanitizeBody('email').trim().escape(),
-  sanitizeBody('phone').trim().escape()
+  sanitizeBody('phone').trim().escape(),
+
+  function registerStudent(req, res, next){
+
+
+    //const errors = validationResult(req);
+
+    var student_register = new Student({
+      lastName: req.body.lastName,
+      firstName: req.body.firstName,
+      school: req.body.school,
+      email: req.body.email,
+      timeStamp: new Date,
+      choice1: req.body.choice1,
+      choice2: req.body.choice2,
+      choice3: req.body.choice3,
+      choice4: req.body.choice4,
+      choice5: req.body.choice5,
+    })
+
+    Student.create(student_register)
+    .then(function(student){
+      res.render('success', student_register);
+    })
+  }
   
 ]
