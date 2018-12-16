daa7eb45dc788b5787a103ab39e0defa5dd0f8c8
Ryan <ryanasmith@ksu.edu>
2018-12-16 12:06:36 -0600

    final fix

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -25,28 +25,7 @@ exports.register_get = function(req, res, next){
   }
 
 // Register Create Student Post
-exports.register_post = [
-
-  // Validate fields.
-  body('firstName', 'Please enter your First Name.').isLength({ min: 1 }).trim(),
-  body('lastName', 'Please enter your Last Name.').isLength({ min: 1 }).trim(),
-	body('school', 'Please select your School').isLength({ min: 1 }).trim(),
-	body('email', 'Please enter your E-Mail.').isLength({ min: 1 }).trim(),
-	body('phone', 'Please enter your Phone Number.').isLength({ min: 1 }).trim(),
-	body('choice1', 'Please make fill all choices').isLength({ min: 1 }).trim(),
-	body('choice2', 'Please make fill all choices').isLength({ min: 1 }).trim(),
-	body('choice3', 'Please make fill all choices').isLength({ min: 1 }).trim(),
-	body('choice4', 'Please make fill all choices').isLength({ min: 1 }).trim(),
-	body('choice5', 'Please make fill all choices').isLength({ min: 1 }).trim(),
-
-  // Sanitize fields.
-  sanitizeBody('firstName').trim().escape(),
-  sanitizeBody('lastName').trim().escape(),
-  sanitizeBody('address').trim().escape(),
-  sanitizeBody('email').trim().escape(),
-  sanitizeBody('phone').trim().escape(),
-
-  function registerStudent(req, res, next){
+exports.register_post = function registerStudent(req, res, next){
 
     const errors = validationResult(req);
 
@@ -79,5 +58,3 @@ exports.register_post = [
       }
     })
   }
-  
-]
